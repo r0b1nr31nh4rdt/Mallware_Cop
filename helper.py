963fc4b34bb9009327bedf348e12325b76327612
Robin Reinhardt <r0b1nr31nh4rdt@gmail.com>
2026-04-26 21:48:18 +0200

    I think it is done

--- helper.py
+++ helper.py
@@ -5,14 +5,15 @@ import time
 import hashlib
 import json
 import requests
+from rich.console import Console
+import os
+import shutil
+import subprocess
 
+console = Console()
 
 def get_processes():
     return list(psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_info']))
-
-
-def get_badhash():
-    return hashlib.sha256(r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*".encode()).hexdigest()
 
 
 def load_cache():
@@ -21,6 +22,12 @@ def load_cache():
             return json.load(f)
     except (json.JSONDecodeError, FileNotFoundError):
         return {}
+
+
+def init_cache():
+    if not os.path.exists('cache.json'):
+        with open('cache.json', 'w') as f:
+            json.dump({}, f)
 
 
 def get_filepath(process):
@@ -41,13 +48,24 @@ def get_filehash(filepath):
         return None
 
 
+def get_badhash():
+    return hashlib.sha256(r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*".encode()).hexdigest()
+
+
+def get_process_hash(p, filepath):
+    if p.info['name'] == "badhash.exe":
+        return get_badhash()
+    return get_filehash(filepath) if filepath else None
+
+
+
 def check_virustotal(filehash, api_key):
     url = f"https://www.virustotal.com/api/v3/files/{filehash}"
     headers = {"x-apikey": api_key}
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
-        console.log(f"Fehler beim Abrufen der Daten: {response.status_code}")
+        console.log(f"Error while fetching data: {response.status_code}")
         return None
 
     data = response.json()
@@ -58,3 +76,32 @@ def check_virustotal(filehash, api_key):
         "undetected": stats["undetected"]
     }
 
+
+def kill_suspicious_process(pid):
+    # try to get and kill parent processes - childs get killed with them
+    try:
+        proc = psutil.Process(pid)
+        parent = proc.parent()
+        if parent and parent.pid > 4:  # PID 0 and 4 are system processes
+            parent.kill()
+        else:
+            proc.kill()  # if no parent found, kill process itself
+    except (psutil.AccessDenied, psutil.NoSuchProcess):
+        pass
+
+
+def move_to_quarantine(filepath):
+    if os.path.exists(filepath):
+        os.makedirs("quarantine", exist_ok=True)
+        shutil.move(filepath, "quarantine/")
+    else:
+        console.log("File not found")
+
+
+def suspend_process(pid):
+    proc = psutil.Process(pid)
+    proc.suspend()
+
+def memory_dump(pid):
+    os.makedirs("dumps", exist_ok=True)
+    subprocess.run(["procdump.exe", "-ma", str(pid), "dumps/"])
